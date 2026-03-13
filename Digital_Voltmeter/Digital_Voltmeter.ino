f2a0550b9e3460bf849f54e98cb7ebb78ef97ea6
Shaan <rafin.ahmed.shaan@gmail.com>
2026-03-13 21:14:01 +0600

    Digital_Voltmeter.ino

--- Digital_Voltmeter/Digital_Voltmeter.ino
+++ Digital_Voltmeter/Digital_Voltmeter.ino
@@ -1,5 +1,5 @@
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>    // initializing library for liquid crystal display
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 #define VinPin A1            // ADC pin for voltage input    
@@ -9,7 +9,7 @@ float RawData = 0;
 float ConvertedData = 0;
 float voltage = 0;
 float R1 = 10000;            // Top Resistor of Voltage Divider 
-float R2 = 1000;             // Top Resistor of Voltage Divider
+float R2 = 1000;             // Bottom Resistor of Voltage Divider
 float avgRawdata = 0;
 long sum = 0;
 
@@ -18,7 +18,7 @@ void setup()
   lcd.init();                      
   lcd.backlight();
   lcd.setCursor(1,0);
-  lcd.print("Shanns Digital");
+  lcd.print("Shaan's Digital");   // Printing Out starting meassage
   lcd.setCursor(3,1);
   lcd.print("Voltmeter!");
   delay(2000); 
@@ -35,45 +35,20 @@ void loop()
   }
 
   avgRawdata = sum / sample;
-  ConvertedData = avgRawdata * (5.0 / 1023.0);
+  ConvertedData = avgRawdata * (5.0 / 1023.0);   // Averaging
   voltage = ConvertedData * ((R1 + R2) / R2);
 
   delay(200);
   lcd.setCursor(0,0);
-  lcd.print("ADC Value: ");
+  lcd.print("ADC Value: ");    // Displaying ADC value
   lcd.print(RawData);
   lcd.print("   ");
 
   lcd.setCursor(0,1);
-  lcd.print("Voltage: ");
+  lcd.print("Voltage: ");    // Displaying Voltage
   lcd.print(voltage);
   lcd.print("V   ");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
